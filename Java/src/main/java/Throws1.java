f19c21792bcbe9a8973d06821712ef4bef6611cb
동원 <ai06021@gmail.com>
2024-05-03 14:46:56 +0900

    Read book

---
+++ Java/src/main/java/Throws1.java
@@ -0,0 +1,39 @@
+public class thorws {
+
+        public static void main(String[] args) {
+            try {
+                method1();
+                method2();
+                method3();
+            } catch (Exception e) {
+                System.out.println("An exception occurred: " + e.getMessage());
+            }
+        }
+
+        public static void method1() {
+            try {
+                System.out.println("Method 1 is executing.");
+                throw new Exception("Exception in Method 1");
+            } catch (Exception e) {
+                System.out.println("Caught an exception in Method 1: " + e.getMessage());
+            }
+        }
+
+        public static void method2() {
+            try {
+                System.out.println("Method 2 is executing.");
+                throw new Exception("Exception in Method 2");
+            } catch (Exception e) {
+                System.out.println("Caught an exception in Method 2: " + e.getMessage());
+            }
+        }
+
+        public static void method3() {
+            try {
+                System.out.println("Method 3 is executing.");
+                throw new Exception("Exception in Method 3");
+            } catch (Exception e) {
+                System.out.println("Caught an exception in Method 3: " + e.getMessage());
+            }
+        }
+}
---
+++ Java/src/main/java/Throws2.java
@@ -0,0 +1,26 @@
+public class Error {
+    public static void main(String[] args) {
+        try {
+            method1();
+            method2();
+            method3();
+        } catch (Exception e) {
+            System.out.println("An exception occurred: " + e.getMessage());
+        }
+    }
+
+    public static void method1() throws Exception {
+        System.out.println("Method 1 is executing.");
+        throw new Exception("Exception in Method 1");
+    }
+
+    public static void method2() throws Exception {
+        System.out.println("Method 2 is executing.");
+        throw new Exception("Exception in Method 2");
+    }
+
+    public static void method3() throws Exception {
+        System.out.println("Method 3 is executing.");
+        throw new Exception("Exception in Method 3");
+    }
+}
